1a7b8b34f6c7c5b54d77f87f0b977b3a3f3d9160
indrek tamme <indrek.tammeste@gmail.com>
2020-06-16 00:52:46 +0300

    db

--- surf24/__init__.py
+++ surf24/__init__.py
@@ -8,16 +8,17 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "superpass"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 login_manager = LoginManager()
 from surf24.core.views import core
 from surf24.users.views import users
 app.register_blueprint(core)
 app.register_blueprint(users)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 Migrate(app,db)
--- surf24/models.py
+++ surf24/models.py
@@ -21,8 +21,9 @@ class condition(Enum):
     NEW=3
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, index=True)
-    username = db.Column(db.String, unique=True, index=True)
+    username = db.Column(db.String(20), unique=True, index=True)
     email = db.Column(db.String(30), unique=True, index=True)
     phone = db.Column(db.String(15), unique=True, index=True)
     password_hash = db.Column(db.String(128))
@@ -31,19 +32,21 @@ class User(db.Model, UserMixin):
     adverts = db.relationship('Advert', backref='author', lazy=True)
 
 class Advert(db.Model):
+    __tablename__ = 'adverts'
     users = db.relationship(User)
     id = db.Column(db.Integer, primary_key=True, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     title = db.Column(db.String(50), nullable=False)
     text = db.Column(db.String(500), nullable=False)
-    price = db.Column()
+    pric = db.Column(db.Integer)
     condition = db.Column(db.Enum(condition))
     location = db.Column(db.String(20))
     hide = db.Column(db.Boolean, default=False, nullable=False)
     images = db.relationship('Image', backref='picture', lazy=True)
 
 class Image(db.Model):
+    __tablename__ = 'images'
     adverts = db.relationship(Advert)
     id = db.Column(db.Integer, primary_key=True, index=True)
     advert_id = db.Column(db.Integer, db.ForeignKey('adverts.id'), nullable=False)
