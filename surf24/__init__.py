48c9fd4980c15099ccf4c02ed78af7f7b45e2d62
indrek tammeste <promenad@gmail.com>
2020-10-16 16:27:48 +0300

    translate

--- surf24/__init__.py
+++ surf24/__init__.py
@@ -72,8 +72,10 @@ from surf24.users.views import users
 from surf24.users.views import google_blueprint
 from surf24.ads.views import ads
 from surf24.categories.views import categories
+from surf24.error_pages.handlers import error_pages
 #from surf24.multilingual import multilingual
 #app.register_blueprint(multilingual)
+app.register_blueprint(error_pages)
 app.register_blueprint(core)
 app.register_blueprint(users)
 app.register_blueprint(google_blueprint, url_prefix="/google_login")
--- surf24/users/views.py
+++ surf24/users/views.py
@@ -89,9 +89,8 @@ def account():
     profile_image = url_for('static', filename= os.path.join('profile_pics', current_user.profile_image))
     return render_template('account.html', profile_image=profile_image, form=form, current_user=current_user)
 
-@users.route("/<username>")
+@users.route("/user/<username>")
 def user_posts(username):
-    return "1"
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
     ads = Advert.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
